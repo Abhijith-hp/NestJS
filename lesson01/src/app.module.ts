3979d8812ce0140e654e30e6cd02224d094a4c2e
ABHIJITH G <20309@gtl.loc>
2026-03-20 14:12:39 +0530

    feat:Add throttle rate limiting

--- lesson01/src/app.module.ts
+++ lesson01/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])],
   controllers: [AppController],
   providers: [AppService],
 })
